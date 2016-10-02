99d84346bc145a4a144c9a7d2b4d70129ea7d2e2
brunogianni <bruno.gianni@resultadosdigitais.com.br>
2016-10-02 17:47:34 -0300

    create load file method

--- lib/roomba.rb
+++ lib/roomba.rb
@@ -1,5 +1,14 @@
-require "roomba/version"
-
 module Roomba
   # Your code goes here...
+  autoload :Room,   'roomba/room'
+  autoload :Hoover, 'roomba/hoover'
+  autoload :Simulation, 'roomba/simulation'
+
+  def load_file(filepath)
+    file_lines = []
+    File.readlines(filepath).map do |line|
+      file_lines << line.split
+    end
+    file_lines
+  end
 end
