b1b8c379fd13b975353e6332705e5e73372de158
brunogianni <bruno.gianni@resultadosdigitais.com.br>
2016-10-02 17:46:09 -0300

    create spec for load file method

--- spec/roomba_spec.rb
+++ spec/roomba_spec.rb
@@ -1,11 +1,18 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Roomba do
-  it "has a version number" do
-    expect(Roomba::VERSION).not_to be nil
+  let(:dummy_class) { Class.new { include Roomba } }
+  let(:valid_filepath) { 'spec/fixtures/files/input.txt' }
+  let(:expected_array) do
+    [%w(5 5), %w(1 2), %w(1 0),
+     %w(2 2), %w(2 3), ['NNESEESWNWW']]
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context '.load_file' do
+    context 'when providing a valid input file' do
+      it 'reads correctly the input file' do
+        expect(dummy_class.new.load_file(valid_filepath)).to eq expected_array
+      end
+    end
   end
 end
